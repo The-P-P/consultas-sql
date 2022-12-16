14106f89a35f2f826bf76996f628cea34710c34d
Pedro Amorim <86755845+Diposu@users.noreply.github.com>
2022-12-15 21:20:09 -0300

    like

---
+++ condicionalLike.sql
@@ -0,0 +1,6 @@
+SELECT * FROM tab WHERE campo LIKE '<condição>';
+
+SELECT * FROM tabela_de_produtos WHERE SABOR LIKE '%Maça%';
+
+SELECT * FROM tabela_de_produtos WHERE SABOR LIKE '%Maça%' AND
+EMBALAGEM = 'PET';
