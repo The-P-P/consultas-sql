bc230d8675418a9677f9070ea29df5c1863bbb04
Pedro Amorim <86755845+Diposu@users.noreply.github.com>
2022-12-17 20:45:30 -0300

    aula 5

---
+++ funcoesstr.sql
@@ -0,0 +1,40 @@
+SELECT CONCAT("SQL ", "Tutorial ", "is ", "fun!") AS ConcatenatedString; 
+
+SELECT LTRIM("     SQL Tutorial") AS LeftTrimmedString;
+
+SELECT RTRIM("SQL Tutorial     ") AS RightTrimmedString;
+
+SELECT TRIM('    SQL Tutorial    ') AS TrimmedString;
+
+SELECT LCASE("SQL Tutorial is FUN!");
+
+SELECT LOWER("SQL Tutorial is FUN!");
+
+SELECT UCASE("SQL Tutorial is FUN!");
+
+SELECT UPPER("SQL Tutorial is FUN!");
+
+SELECT SUBSTRING("SQL Tutorial", 5, 3) AS ExtractString;
+
+SELECT LENGTH("SQL Tutorial") AS LengthOfString;
+
+SELECT LTRIM('    OLÁ')
+
+SELECT LTRIM('    OLÁ') AS RESULTADO;
+
+SELECT RTRIM('OLÁ     ') AS RESULTADO;
+
+SELECT TRIM('    OLÁ    ') AS RESULTADO;
+
+SELECT CONCAT('OLÁ', ' ', 'TUDO BEM','?') AS RESULTADO;
+
+SELECT UPPER('olá, tudo bem?') AS RESULTADO;
+
+SELECT LOWER('OLÁ, TUDO BEM?') AS RESULTADO;
+
+SELECT SUBSTRING('OLÁ, TUDO BEM?', 6) AS RESULTADO;
+
+SELECT SUBSTRING('OLÁ, TUDO BEM?', 6, 4) AS RESULTADO;
+
+SELECT CONCAT(NOME, ' (', CPF, ') ') AS RESULTADO FROM TABELA_DE_CLIENTES;
+
